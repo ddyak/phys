05697bfc7f82ef007774535c54d8329f4339a886
Daniil Yakovlev <ddyakovlev@yandex.ru>
2019-12-14 17:17:06 +0700

    [AVR] am2320 doesn't work

--- avr-2019/5/i2c.c
+++ avr-2019/5/i2c.c
@@ -3,16 +3,13 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-
-char data;
-
+#include <stdio.h>
 
 void USART_Init() {
 	UBRR0H = 0;
 	UBRR0L = 103;
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0) | (0 << TXCIE0);
 	UCSR0C = (1 << USBS0) | (3 << UCSZ00);
-	sei();
 }
 
 void USART_Transmit(char data) {
@@ -29,92 +26,123 @@ void UART_Send_Str(const char* str) {
 	USART_Transmit('\n');
 }
 
-int USART_Receive() {
-	// while (!(UCSR0A & (1 << RXC0)));
-	return UDR0;
-}
-
-ISR( USART0_RX_vect ) {
-	cli();
-	unsigned char rxbyte = USART_Receive();
-	UART_Send_Str("ISR");
-	if (rxbyte == '0') PORTB = 0;
-	if (rxbyte == '1') PORTB = 128;
-	sei();
-//	reti();
-}
-
 void i2cInit(){
-	TWBR = 5;
-	TWSR = (0<<TWPS1)|(0<<TWPS0);
+	TWBR = 8;
+	TWSR = (0 << TWPS1) | (0 << TWPS0);
 }
 
 void i2cStop(){
-	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
+	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
 }
 
 void i2cStart(){
-	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
-	while (!(TWCR & (1<<TWINT)));
+	TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+	while (!(TWCR & (1 << TWINT)));
 }
 
 char i2cReciveLast() {
-	TWCR = (1<<TWINT|1<<TWEN);
-	while (!(TWCR & (1<<TWINT)));
+	TWCR = (1 << TWINT) | (1 << TWEN);
+	while (!(TWCR & (1 << TWINT)));
 	return TWDR;
 }
 
 char i2cRecive(){
-	TWCR = (1<<TWINT|1<<TWEN|1<<TWEA);
-	while (!(TWCR & (1<<TWINT)));
+	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA);
+	while (!(TWCR & (1 << TWINT)));
 	return TWDR;
 }
 
 void i2cSend(char send){
 	TWDR = send;
-	TWCR = (1<<TWINT)|(1<<TWEN);
-	while (!(TWCR & (1<<TWINT)));
+	TWCR = (1 << TWINT) | (1 << TWEN);
+	while (!(TWCR & (1 << TWINT)));
 }
 
-void i2cSendStr(char str[]){
+void i2cSendStr(const char* str) {
 	unsigned char i = 0;
-
-	while (str[i]!='\0')
-	{
+	while (str[i] != '\0') {
 		i2cSend(str[i]);
 		i++;
 	}
 }
 
-void main(void){
-	data = '0';
-	char addr = 0b10100010;
-	char recieveData;
-	USART_Init();
+unsigned int CRC16(unsigned char *ptr, unsigned char length) 
+{ 
+      unsigned int crc = 0xFFFF; 
+      uint8_t s = 0x00; 
+
+      while(length--) {
+        crc ^= *ptr++; 
+        for(s = 0; s < 8; s++) {
+          if((crc & 0x01) != 0) {
+            crc >>= 1; 
+            crc ^= 0xA001; 
+          } else crc >>= 1; 
+        } 
+      } 
+      return crc; 
+} 
+
+// const char device_addr = 0b10100010;
+const char device_addr = 0xB8;
+
+int getTem() {
+	unsigned char buf[8];
+	for(int s = 0; s < 8; s++) buf[s] = 0x02;
+
+	i2cStart();
+	i2cSend(device_addr); // 0-w, 1-r
+	i2cSend(0x03); // Function code - read data//low temp
+	i2cSend(0x00);
+	i2cSend(0x04);
+	i2cStop();
+		
+	_delay_ms(0.01);
+
+	i2cStart();
+	i2cSend(device_addr + 1); // 0-w, 1-r
+
+	for (int i = 0; i < 0x08; i++) {
+		if (i == 7) 
+			buf[i] = i2cReciveLast();
+		else 
+			buf[i] = i2cReciveLast();
+	}	
+	i2cStop();
+
+	char* str = "0000000000000000000000000000000000000000000000000000";
+	sprintf(str, "%d, %d, %d, %d, %d, %d, %d, %d", 
+	buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7]);
+	UART_Send_Str(str);
+
+	// unsigned int Rcrc = buf[7] << 8;
 	
+	// float t, h;
+	
+	// Rcrc += buf[6];
+
+	// // if (Rcrc == CRC16(buf, 6)) {
+	// 	unsigned int temperature = ((buf[4] & 0x7F) << 8) + buf[5];
+	// 	t = temperature / 10.0;
+	// 	t = ((buf[4] & 0x80) >> 7) == 1 ? t * (-1) : t;
+
+	// 	unsigned int humidity = (buf[2] << 8) + buf[3];
+	// 	h = humidity / 10.0;
+	// 	char* str = "0000000000000000000000000000000000000000000";
+	// 	sprintf(str, "{%d, %d, %d, %d, %d, %d, %d, %d}", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7]);
+	// 	UART_Send_Str(str);
+	// } else {
+		// UART_Send_Str("WRONG CRC");
+	// }
+}
+
+void main(void) {
+	USART_Init();
 	i2cInit();
 	
-	while(1)
- 	{
-		i2cStart();
-		i2cSend(addr);
-		i2cSend(0);
-		i2cSendStr("hello world");
-		i2cStop();
-		
-	   	_delay_ms(5000);
-		
-		i2cStart();
-		i2cSend(addr);
-		i2cSend(0);
-		i2cStart();
-		i2cSend(addr+1);
-		for(int i = 1; i < 10; i++){
-			recieveData = i2cRecive();
-			USART_Transmit(recieveData);
-	    }
-	    recieveData = i2cReciveLast();
-	    USART_Transmit(recieveData);
-		i2cStop();
+	while(1) {
+		// UART_Send_Str("New cirle");
+		getTem();
+	   	_delay_ms(1000);
 	}
 }
